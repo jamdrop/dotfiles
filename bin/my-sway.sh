d8e4a71a07bbfba0c0fb58d6c6b77316d6649441
Jakob Mayring <jam@jammm.eu.org>
2018-01-01 15:13:07 +0100

    inverted horizontal and vertical split do be more keyboard intuitive

--- bin/my-sway.sh
+++ bin/my-sway.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# gui toolkit stuff
 export GDK_BACKEND=wayland
 export QT_QPA_PLATFORM=wayland-egl
 export CLUTTER_BACKEND=wayland
@@ -6,5 +8,10 @@ export SDL_VIDEODRIVER=wayland
 export ECORE_EVAS_ENGINE=wayland_egl
 export ELM_DISPLAY=wl
 export ELM_ACCEL=opengl
+
+
+# wayland / sway config
 export XKB_DEFAULT_LAYOUT=de
-sway
+export WLC_SHM=1
+export WLC_DRM_DEVICE=card0
+sway --debug $@ 2>&1 | tee /tmp/sway.log
